88ae8b7d15fe75726edc9275a20ea3169f30787f
davidmitchellfootball <davidmitchellfootball@googlemail.com>
2025-06-26 21:49:23 +0100

    rm comment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['@/assets/css/main.css'], // ✅ This is correct
+  css: ['@/assets/css/main.css'], 
   modules: ['@nuxtjs/tailwindcss'],
 })
