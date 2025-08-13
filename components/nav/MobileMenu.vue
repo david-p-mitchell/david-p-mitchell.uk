62f943279e48df85b0354e0600f33837cc6a3671
David <davidmitchellfootball@googlemail.com>
2025-08-13 17:42:03 +0100

    changing around navigation for mobile

---
+++ components/nav/MobileMenu.vue
@@ -0,0 +1,41 @@
+
+<script setup lang="ts">
+defineProps({
+  isOpen: {
+    type: Boolean,
+    default: false
+  }, toggleMenu: {
+    type: Function,
+    required: true
+  }
+})
+import MenuIcon from './MenuIcon.vue'
+</script>
+
+<template> 
+    <div class="md:hidden">
+        <button
+        @click="(e) => toggleMenu(e)"
+        class="text-white hover:text-blue-300 focus:outline-none focus:text-blue-300 transition-colors duration-200"
+        aria-label="Toggle menu"
+        >
+        <template v-if="isOpen">
+            <!-- X icon -->
+            <svg
+            class="h-6 w-6"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
+            </svg>
+        </template>
+        <template v-else>
+            <!-- Menu icon -->
+            <MenuIcon />
+        </template>
+        </button>
+    </div>
+</template>
--- components/nav/NavBar.vue
+++ components/nav/NavBar.vue
@@ -2,7 +2,8 @@
 import { ref } from 'vue'
 import NavLogo from './NavLogo.vue'
 import NavItem from './NavItem.vue'
-import MobileMenu from './NavMobileMenu.vue'
+import MobileMenu from './MobileMenu.vue'
+import NavMobileMenu from './NavMobileMenu.vue'
 
 const isOpen = ref(false)
 const activeDropdown = ref(null)
@@ -47,11 +48,9 @@ const menuItems = [
 
 <template>
   <nav class="bg-blue-950 text-white shadow-lg border-b border-gray-200">
-    <div class="mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="mx-auto px-4 ">
       <div class="flex justify-between items-center h-8">
         <NavLogo />
-
-        <!-- Desktop Menu -->
         <div class="hidden md:block">
           <div class="ml-10 flex items-baseline space-x-4">
             <NavItem
@@ -66,50 +65,20 @@ const menuItems = [
         </div>
 
         <!-- Mobile menu button -->
-        <div class="md:hidden">
-          <button
-            @click="toggleMenu"
-            class="text-white hover:text-blue-300 focus:outline-none focus:text-blue-300 transition-colors duration-200"
-            aria-label="Toggle menu"
-          >
-            <template v-if="isOpen">
-              <!-- X icon -->
-              <svg
-                class="h-6 w-6"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
-              </svg>
-            </template>
-            <template v-else>
-              <!-- Menu icon -->
-              <svg
-                class="h-6 w-6 mt-1"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16"></path>
-              </svg>
-            </template>
-          </button>
-        </div>
+         <MobileMenu 
+          :isOpen="isOpen" :toggleMenu="toggleMenu"
+           />
       </div>
     </div>
     
 
     <!-- Mobile Menu -->
-    <MobileMenu
+    <NavMobileMenu
       :menuItems="menuItems"
       :activeDropdown="activeDropdown"
       :toggleDropdown="toggleDropdown"
       v-if="isOpen"
     />
+    
   </nav>
 </template>
