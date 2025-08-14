c1f376e8d55c0d40bc19e058b8c833444c9c37c9
David <davidmitchellfootball@googlemail.com>
2025-08-14 13:10:21 +0100

    chore: upgrade nuxt to version 4.0.3 and update tailwindcss dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,14 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['@/assets/css/main.css'], 
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+      'postcss-imported': {},
+      'tailwindcss/nesting': {}
+    }
+  },
   modules: ['@nuxtjs/tailwindcss'],
   ssr: true,
   tailwindcss: {
