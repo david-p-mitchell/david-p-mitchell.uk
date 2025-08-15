a0c7c35845b44638e7d50411eb923d84cf14fe5f
David <davidmitchellfootball@googlemail.com>
2025-08-15 04:41:35 +0100

    adding dates

--- components/projects/ProjectComponent.vue
+++ components/projects/ProjectComponent.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="flex justify-center mt-4 max-w-sm">
-          <div class="flex flex-wrap justify-center gap-4">
-            <div class="bg-white p-6 rounded-lg shadow-md text-center transform transition-transform duration-300 hover:scale-105">
-            
-            <h2 class="text-xl font-semibold mb-2">{{ heading }}</h2>
-            <p class="text-gray-600 text-lg">{{ subheading }}</p>
-            <p class="text-black-600 text-sm p-2 m-4">{{ summary }}</p>
-            <slot/>
-          </div>
-        </div>    
-          </div>    
+  <div class="flex justify-center mt-4 max-w-sm">
+    <div class="flex flex-wrap justify-center gap-4">
+      <div class="relative bg-white p-6 rounded-lg shadow-md text-center transform transition-transform duration-300 hover:scale-105">
+        <h2 class="text-xl font-semibold mb-1 pt-1">{{ heading }}</h2>
+        <p class="text-gray-600 text-md p-0 mb-4">{{ dates }}</p>
+        <p class="text-gray-600 text-lg p-0">{{ subheading }}</p>
+        <p class="text-black-600 text-sm p-0 m-4">{{ summary }}</p>
+        <slot/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -25,6 +25,10 @@ defineProps({
     summary: {
         type: String,
         default: 'Styled with Tailwind CSS for a modern look.'
+    },
+    dates: {
+        type: String,
+        default: ''
     }
 })
 </script>
