35b23f0950ec6676ee9c24eb54d34034f9a9eaec
David <davidmitchellfootball@googlemail.com>
2025-08-15 03:16:30 +0100

    change home page display

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,41 @@
         </div>
       </div>
       <About />
+      <div class="flex flex-wrap justify-center items-center"> 
+        <LanguagesBash :size="size" />
+        <LanguagesCSharp :size="size" />
+        <LanguagesCss :size="size" />
+        <LanguagesNuxt :size="size"/>
+        <LanguagesNextJs :size="size"/>
+        <LanguagesHtmlFive :size="size"/>
+        <LanguagesJQuery :size="size"/>
+        <LanguagesJson :size="size"/>
+        <LanguagesMarkdown :size="size"/>
+        <LanguagesNodeJs :size="size"/>
+        <LanguagesRequireJs :size="size"/>
+        <LanguagesReact :size="size"/>
+        
+        <LanguagesSQL :size="size"/>
+        <LanguagesJavascript :size="size"/>
+        <LanguagesTypescript :size="size"/>
+        <LanguagesVue :size="size"/>
+        <LanguagesXML :size="size"/>
+        <LanguagesYAML :size="size"/>
+        
+        
+        <PlatformsAws :size="size" />
+        <PlatformsDocker :size="size" />
+        <PlatformsDotNetCore :size="size" />
+        <PlatformsDotNet :size="size" />
+        <PlatformsGit :size="size" />      
+      </div> 
+      <div class="text-center mt-12 mb-6">
+        Some Projects I have worked on:
+        <div class="flex justify-center mt-4">
+        <Projects />  
+        </div>    
+      </div>
+      
       <footer class="mt-12">
         <Footer />
       </footer>
@@ -14,7 +49,11 @@
 </template>
 
 <script setup>
+import { useHead } from '@vueuse/head';
+import Footer from '@/components/Footer.vue';
 import About from '@/components/About.vue';
+import Projects from '~/components/projects/Projects.vue';
+const size = 70;
 
 useHead({
   title: 'David Mitchell',
