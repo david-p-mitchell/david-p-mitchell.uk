87f57dc5226e9943956c0ad1f7c3fcd26945ab6e
David <davidmitchellfootball@googlemail.com>
2025-08-15 04:51:36 +0100

    only 2 projs atm so 2 cols

--- components/projects/Projects.vue
+++ components/projects/Projects.vue
@@ -5,7 +5,7 @@ import Halliburton from './Halliburton.vue';
 </script>
 
 <template>
-    <div class="grid md:grid-cols-3 flex-col items-center justify-center p-4 gap-6">
+    <div class="grid md:grid-cols-2 flex-col items-center justify-center p-4 gap-6">
         
         
         <!-- Project Components -->
