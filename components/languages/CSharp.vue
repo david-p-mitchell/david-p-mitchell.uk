6924d9ed4ea23f98d1f1add3db9ffb53c5a06a49
David <davidmitchellfootball@googlemail.com>
2025-08-15 03:10:48 +0100

    feat: add CSharp component with BubbleLogo integration

---
+++ components/languages/CSharp.vue
@@ -0,0 +1,27 @@
+<template>
+    <div style="display: inline-block; position: relative;">
+        <BubbleLogo
+            :name="'C#'"
+            :size="size"
+            :showName="false"
+            :link="link">
+        <text x="50%" y="50%" text-anchor="middle" fill="white" font-family="Arial, sans-serif" font-size="40" font-weight="bold">
+            C#
+        </text>
+        </BubbleLogo>
+    </div>
+</template>
+
+<script setup>
+import BubbleLogo from '@/components/BubbleLogo.vue';
+const props = defineProps({
+    size: {
+        type: [Number, String],
+        default: 96
+    },
+    link: {
+        type: String,
+        default: '/languages/c-sharp'
+    }
+});
+</script>
