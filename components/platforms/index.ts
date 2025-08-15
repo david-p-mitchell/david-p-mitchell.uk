2d77398407c1d52b2deca88471d4272affebe762
David <davidmitchellfootball@googlemail.com>
2025-08-15 03:15:45 +0100

    add platform summary

---
+++ components/platforms/index.ts
@@ -0,0 +1,5 @@
+export { default as AWS } from './Aws.vue';
+export { default as Docker } from './Docker.vue';
+export { default as DotNet } from './DotNet.vue';
+export { default as DotNetCore } from './DotNetCore.vue';
+export { default as Git } from './Git.vue';
