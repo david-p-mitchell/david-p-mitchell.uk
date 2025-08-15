ff0479b3100a5e7bb35577aeb588651424b51e87
David <davidmitchellfootball@googlemail.com>
2025-08-15 04:43:46 +0100

    adding projects

--- components/projects/Projects.vue
+++ components/projects/Projects.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 import ApolloInsights from './ApolloInsights.vue';
+import EnonWebsite from './EnonWebsite.vue';
+import Halliburton from './Halliburton.vue';
 
 </script>
 
 <template>
-    <div>
-    <ApolloInsights />
+    <div class="grid md:grid-cols-3 flex-col items-center justify-center p-4 gap-6">
+        
+        
+        <!-- Project Components -->
+        <EnonWebsite />
+        <ApolloInsights />
+        <Halliburton />
     </div>
 </template>
