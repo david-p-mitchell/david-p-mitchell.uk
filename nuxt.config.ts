d8fcdf9c0e2275dd83f516ec2f6d23275b690d61
David <davidmitchellfootball@googlemail.com>
2025-08-16 14:54:02 +0100

    adding GA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,5 +16,26 @@ export default defineNuxtConfig({
   tailwindcss: {
     cssPath: '~/assets/css/main.css',
     configPath: 'tailwind.config.js'
+  },
+  app: {
+    head: {
+      title: 'David P Mitchell',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Portfolio of David P Mitchell, showcasing web development' },
+        { name: 'keywords', content: 'web development, portfolio, David Mitchell, Nuxt, react, .net, Vue' },
+      ],
+      script: [
+        { src: 'https://www.googletagmanager.com/gtag/js?id=G-CYZQFN61ZP', async: true },
+        {
+          children: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CYZQFN61ZP');
+          `,
+        } as any
+      ]
+    }
   }
 })
