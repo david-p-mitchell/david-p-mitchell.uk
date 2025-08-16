08bcf73cdb8e558ddb6e51f0c9d2b91cd665b88c
David <davidmitchellfootball@googlemail.com>
2025-08-16 15:31:46 +0100

    fix About formatting

--- components/About.vue
+++ components/About.vue
@@ -1,19 +1,25 @@
 <template>
   <div>
     <section class="mx-auto mt-2 p-6 bg-gray-100 rounded-lg">
-      <h1 class="mb-4 text-2xl text-gray-800">About Me</h1>
-      <p class="text-lg text-gray-600 leading-relaxed">
+      <h1 class="mb-4 text-2xl text-gray-800 text-center">About Me</h1>
+      <p class="text-lg text-gray-600 leading-relaxed text-md">
         Hi, I am David, a passionate full stack software developer with a focus on building robust and user-friendly web applications. 
 
         <br />
-        I have worked in the Digital Search Marketing industry for over 8 years, where I have honed my skills in software development and project management.
+        I have worked in the Digital Search Marketing industry for over 8 years, where I have honed my skills in software and web development.
         My great passion is creating technical solutions that solve real-world problems and enhance user experiences.
         Whether that be in time saving, cost saving or just making life easier.
+        <br />
         I have a strong background in software development, with expertise in both front-end and back-end.
         I have experience in various technologies including .NET, C#, Vue.js, and React.js.
+
         I enjoy working with modern JavaScript frameworks like Vue.js and am always eager to learn new
-        technologies. My goal is to create solutions that make a positive impact
+        technologies. 
+        My goal is to create solutions that make a positive impact
         and deliver great user experiences.
+
+        <br />
+        Below are some of the skills and technologies I have worked with:
       </p>
     </section>
   </div>
