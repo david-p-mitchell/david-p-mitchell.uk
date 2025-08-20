b31b3682668351a4a4bdfbac0371812bbb21e05f
David <davidmitchellfootball@googlemail.com>
2025-08-20 16:14:00 +0100

    add margin

--- components/projects/disclaimers/Disclaimer.vue
+++ components/projects/disclaimers/Disclaimer.vue
@@ -1,15 +1,18 @@
 <template>
-        <p class="text-xs mx-2 mb-2">
+  <div class="flex flex-col">
+  <div class="m-auto">
+    <p class="text-xs mx-2 mb-2">
             All company logos and images are the property of {{CompanyNameMap[company]}}. <br />
             They are used here for illustrative purposes to show past employment and projects.
-        </p>  
+        </p>
+  </div>
+</div>
+        
+  
 </template> 
 <script setup lang="ts">
 import { CompanyNameMap } from '@/utils/companyNameMap';
 defineProps<{
   company: 'VL' | 'Halliburton';
 }>();
-
-
-
 </script>
