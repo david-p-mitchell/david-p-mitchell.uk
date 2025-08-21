b8175812c606a3a7d28b99af29e37abe97592ce4
David <davidmitchellfootball@googlemail.com>
2025-08-21 21:17:39 +0100

    rm under construction

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <UnderConstruction />
     <NavBar />
     <main>
       <slot />
@@ -10,6 +9,5 @@
 
 <script setup>
 import NavBar from '~/components/nav/NavBar.vue'
-import UnderConstruction from '~/components/UnderConstruction.vue';
 // You can add layout-specific logic here if needed
 </script>
