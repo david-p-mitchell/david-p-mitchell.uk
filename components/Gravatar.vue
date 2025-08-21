dce7ca9c66af073a6c96f6db1e6a83ec8db01706
David <davidmitchellfootball@googlemail.com>
2025-08-21 19:41:32 +0100

    adding Gravatar

---
+++ components/Gravatar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex justify-center items-center">
+    <img :src="avatarUrl" alt="User Avatar" class="rounded-full" />
+  </div>
+</template>
+
+<script setup>
+import { getGravatarUrl } from '~/utils/gravatar'
+
+const email = 'davidpmitchell92@googlemail.com'
+const avatarUrl = getGravatarUrl(email, 250)
+</script>
