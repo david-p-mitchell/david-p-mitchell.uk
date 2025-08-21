3b53d8c9e4005962a020fb941d5f1074e641b614
David <davidmitchellfootball@googlemail.com>
2025-08-21 19:45:27 +0100

    reformating proj cards

--- components/projects/Cards/ProjectCards.vue
+++ components/projects/Cards/ProjectCards.vue
@@ -1,36 +1,23 @@
 <template>
-  <div class="flex flex-col justify-center mt-4 max-w-sm">
-    <div class="flex justify-center gap-4 flex-nowrap">
-      <div class="relative bg-white rounded-lg shadow-md text-center transform transition-transform duration-300 hover:scale-105 overflow-hidden">
-        
-        <EmployedHeaders v-if="employed" :type="company!" />
-        <VolunteerHeader v-else />
+    <div class="flex flex-col justify-center mt-4 max-w-sm p-1">
+    <div class="flex flex-wrap justify-center gap-6">
+      <div class="relative justify-center bg-white rounded-lg shadow-md text-center transform transition-transform duration-300 hover:scale-105">
+    <EmployedHeaders v-if="employed" :type="company!" />
+    <VolunteerHeader v-else />
 
-        <h2 class="text-xl font-semibold p-2 whitespace-nowrap overflow-hidden text-ellipsis">{{ heading }}</h2>
-        <p class="text-gray-600 text-md p-0 mb-4 whitespace-nowrap overflow-hidden text-ellipsis">{{ dates }}</p>
-        <p class="text-gray-600 text-lg p-2 whitespace-nowrap overflow-hidden text-ellipsis">{{ subheading }}</p>
-        <p class="text-black-600 text-sm px-2 mt-2 mx-1 mb-1 whitespace-nowrap overflow-hidden text-wrap flex items-center justify-center">{{ summary }}</p>
-
-        <div class="h-[125px] flex items-center justify-center  whitespace-nowrap overflow-hidden">
-          <TechIcons
-            v-if="tech && tech.length"
-            :tech="tech"
-            :iconsize="iconsize"
-            :isSearchIcon="false"
-            class="py-2"
-          />
-        </div>
-
-        <Disclaimer v-if="employed" :company="company!" />
-      </div>
+    <h2 class="text-xl font-semibold pt-1">{{ heading }}</h2>
+    <p class="text-gray-600 text-sm p-0">{{ '(' + dates + ')' }}</p>
+    <p class="text-gray-600 text-lg p-2">{{ subheading }}</p>
+    <p class="text-black-600 text-sm px-2 mt-2 mx-1 mb-1">{{ summary }}</p>
+    
+    <TechIcons v-if="tech && tech.length" :tech="tech" :iconsize="iconsize" :isSearchIcon="false" />
+    </div>
     </div>
   </div>
 </template>
-
 <script setup lang="ts">
 import EmployedHeaders from '../CardHeaders/EmployedHeader.vue';
 import VolunteerHeader from '../CardHeaders/VolunteerHeader.vue';
-import Disclaimer from '../disclaimers/Disclaimer.vue';
 
 defineProps({
   heading: {
@@ -39,7 +26,7 @@ defineProps({
     required: true
   },
   employed: { type: Boolean, default: true},
-  company: { type: String as () => 'VL' | 'Halliburton' | null |undefined, required: true },
+  company: { type: String as () => 'VL' | 'Halliburton' | 'VLGain' | null |undefined, required: true },
     subheading: {
         type: String,
         default: 'Some Projects I have worked on:',
