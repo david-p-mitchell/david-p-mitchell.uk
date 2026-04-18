23fe0a75abe20c384fb82565b880efebbc7ee247
davidmitchellfootball <davidmitchellfootball@googlemail.com>
2026-04-18 23:37:57 +0100

    Update Tailwind CSS PostCSS plugin in configuration and package files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   postcss: {
     plugins: {
-      tailwindcss: {},
+      '@tailwindcss/postcss': {},
       autoprefixer: {}
     }
   },
