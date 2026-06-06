8b73d568ec931c048761b4219f49845940f97ec0
davidmitchellfootball <davidmitchellfootball@googlemail.com>
2026-06-06 23:37:57 +0100

    add me data

---
+++ src/data/me.ts
@@ -0,0 +1,6 @@
+export const me = {
+  name: "David Mitchell",
+  role: "Developer · Christian",
+  bio: "I love when we can thoughtfully infuse tech and faith together. I love helping others with the tools I build and building others up.",
+  imageSrc: "/images/Me.jpg",
+};
