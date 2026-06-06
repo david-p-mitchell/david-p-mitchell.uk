7abfac1629333f4799b5955da2894cd2c45c3949
davidmitchellfootball <davidmitchellfootball@googlemail.com>
2026-06-06 23:45:16 +0100

    add verses

---
+++ src/data/blogs.ts
@@ -0,0 +1,9 @@
+import type { Link } from "../types/link"
+export const Blogs : Link[]=[
+    { name: "Challies", url: "https://www.challies.com" },
+    { name: "Ligonier Ministries", url: "https://www.ligonier.org" },
+      { name: "The Gospel Coalition", url: "https://www.thegospelcoalition.org" }
+]
+    
+
+
---
+++ src/data/verses.ts
@@ -0,0 +1,9 @@
+export const verses = [
+  { verse: "I can do all things through Christ who strengthens me.", reference: "Philippians 4:13" },
+  { verse: "Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight.", reference: "Proverbs 3:5–6" },
+  { verse: "For I know the plans I have for you, declares the Lord, plans to prosper you and not to harm you, plans to give you hope and a future.", reference: "Jeremiah 29:11" },
+  { verse: "Be still, and know that I am God.", reference: "Psalm 46:10" },
+  { verse: "The Lord is my shepherd; I shall not want.", reference: "Psalm 23:1" },
+  { verse: "Do not be anxious about anything, but in every situation, by prayer and petition, with thanksgiving, present your requests to God.", reference: "Philippians 4:6" },
+  { verse: "And we know that in all things God works for the good of those who love him.", reference: "Romans 8:28" },
+];
