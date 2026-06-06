7b9ef02b53023e59a2b0787d4b2f85826c0f0d44
davidmitchellfootball <davidmitchellfootball@googlemail.com>
2026-06-06 23:38:53 +0100

    add book data

---
+++ src/data/books.ts
@@ -0,0 +1,10 @@
+export const books = [
+  {
+    title: "Willing but Weak",
+    author: "Paul Williams",
+    progress: Math.round((146 / 936) * 100),
+    coverColor: "#2d4a3e",
+    img: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1587471276i/53237787.jpg"
+  }
+
+];
