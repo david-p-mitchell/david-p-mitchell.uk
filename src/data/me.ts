d98cadd7c858789ec5474a28505096293828ba67
David <davidmitchellfootball@googlemail.com>
2026-06-09 14:28:37 +0100

    Change order

--- src/data/me.ts
+++ src/data/me.ts
@@ -1,6 +1,6 @@
 export const me = {
   name: "David Mitchell",
-  role: "Developer · Christian",
+  role: "Christian · Developer",
   bio: "I love when we can thoughtfully infuse tech and faith together. I love helping others with the tools I build and building others up. ",
   siteInfo: "My hope is that this site will be an encouragement to you and that you will find some helpful resources here.",
   disclaimer: "This site is a personal project and the views expressed here are my own and do not necessarily reflect those of any organization I am affiliated with.",
