c2cf960b7f095595f9b57253e84bdefd0007518a
davidmitchellfootball <davidmitchellfootball@googlemail.com>
2026-06-10 16:24:35 +0100

    Updating reading books

--- src/data/books.ts
+++ src/data/books.ts
@@ -5,6 +5,20 @@ export const books = [
     progress: Math.round((146 / 936) * 100),
     coverColor: "#2d4a3e",
     img: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1587471276i/53237787.jpg"
+  },
+  {
+    title: "Foundations: Four Big Questions We Should Be Asking But Typically Don't",
+    author: "Peter Mead",
+    progress: Math.round((1 / 96) * 100),
+    coverColor: "#2d4a3e",
+    img: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1438971069i/26058246.jpg"
+  },
+  {
+    title: "What is Jesus Doing Now?",
+    author: "Gary Brady",
+    progress: Math.round((8 / 224) * 100),
+    coverColor: "#cfcfcf",
+    img: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1355869855i/15938724.jpg"
   }
 
 ];
