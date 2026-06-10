1de40cc57abfebff92584410a7b42846438999e9
davidmitchellfootball <davidmitchellfootball@googlemail.com>
2026-06-10 16:39:25 +0100

    add tag data

--- src/data/books.ts
+++ src/data/books.ts
@@ -4,21 +4,25 @@ export const books = [
     author: "Paul Williams",
     progress: Math.round((146 / 936) * 100),
     coverColor: "#2d4a3e",
-    img: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1587471276i/53237787.jpg"
+    img: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1587471276i/53237787.jpg",
+    tags: ["Spiritual Growth", "Discipleship"]
+
   },
   {
     title: "Foundations: Four Big Questions We Should Be Asking But Typically Don't",
     author: "Peter Mead",
     progress: Math.round((1 / 96) * 100),
     coverColor: "#2d4a3e",
-    img: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1438971069i/26058246.jpg"
+    img: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1438971069i/26058246.jpg",
+    tags: ["Foundations", "Four Big Questions", "Relationship"]
   },
   {
     title: "What is Jesus Doing Now?",
     author: "Gary Brady",
     progress: Math.round((8 / 224) * 100),
     coverColor: "#cfcfcf",
-    img: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1355869855i/15938724.jpg"
+    img: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1355869855i/15938724.jpg",
+    tags: ["Jesus"]
   }
 
 ];
