bc5ee37f45a738225969e569e1af0155dbd11c48
David <davidmitchellfootball@googlemail.com>
2026-06-11 12:20:33 +0100

    update books

--- src/data/books.ts
+++ src/data/books.ts
@@ -5,7 +5,8 @@ export const books = [
     progress: Math.round((146 / 936) * 100),
     coverColor: "#2d4a3e",
     img: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1587471276i/53237787.jpg",
-    tags: ["Spiritual Growth", "Discipleship"]
+    tags: ["Spiritual Growth", "Discipleship"],
+    show: false
 
   },
   {
@@ -22,7 +23,25 @@ export const books = [
     progress: Math.round((8 / 224) * 100),
     coverColor: "#cfcfcf",
     img: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1355869855i/15938724.jpg",
-    tags: ["Jesus"]
+    tags: ["Jesus"],
+    show: false
+  },
+  {
+    title: "The 1689 Handbook: A Chapter-by-Chapter Introduction to the Second London Baptist Confession of Faith",
+    author: "Robert Strivens",
+    progress: Math.round((0 / 224) * 100),
+    coverColor: "#cfcfcf",
+    img: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1774114537i/250080968.jpg",
+    tags: ["Confession of Faith", "1689", "Baptist", "Reformed"]
+  },
+  {
+    title: "Power through Prayer",
+    author: "E. M. Bounds",
+    progress: Math.round((0 / 224) * 100),
+    coverColor: "#cfcfcf",
+    img: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1328823045i/6594958.jpg",
+    tags: ["Prayer", "Spiritual Growth"],
   }
+
 
 ];
