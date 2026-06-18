ce1bad1eea83f274814ab0b4c76186843509e631
David <davidmitchellfootball@googlemail.com>
2026-06-18 14:41:56 +0100

    adding data section

---
+++ src/data/bibleReading.ts
@@ -0,0 +1,15 @@
+
+export type Reading = {
+  book: string;
+  progress: number;
+  lastRead: Date | string;
+};
+const readings : Reading[] = [
+    // {
+    //     book: "Jude",
+    //     progress: 100,
+    //     lastRead: "2024-06-01"
+    // }
+]
+
+export default readings
