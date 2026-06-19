72746f54d81e45db2f895dc3b4d33b32f146cf75
David <davidmitchellfootball@googlemail.com>
2026-06-19 12:42:08 +0100

    update progress

--- src/data/bibleReading.ts
+++ src/data/bibleReading.ts
@@ -9,7 +9,13 @@ const readings : Reading[] = [
          book: "Philemon",
          progress: 100,
          lastRead: "2026-06-18"
+     },
+     {
+         book: "Jude",
+         progress: 100,
+         lastRead: "2026-06-19"
      }
+
 ]
 
 export default readings
