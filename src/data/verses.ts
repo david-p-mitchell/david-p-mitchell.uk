44b806d7a69070af19a3ab24fbcfa702fcfa180d
davidmitchellfootball <davidmitchellfootball@googlemail.com>
2026-08-30 22:30:19 +0100

    update verses

--- src/data/verses.ts
+++ src/data/verses.ts
@@ -1,9 +1,9 @@
 export const verses = [
   { verse: "I can do all things through Christ who strengthens me.", reference: "Philippians 4:13" },
   { verse: "Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight.", reference: "Proverbs 3:5–6" },
-  { verse: "For I know the plans I have for you, declares the Lord, plans to prosper you and not to harm you, plans to give you hope and a future.", reference: "Jeremiah 29:11" },
-  { verse: "Be still, and know that I am God.", reference: "Psalm 46:10" },
+  { verse: "For I know the plans I have for you, declares the LORD, plans for welfare and not for evil, xto give you a future and a hope. Then you will call upon me and come and pray to me, and I will hear you. You will seek me and find me, when you seek me with all your heart.", reference: "Jeremiah 29:11-13" },
+  { verse: "“Be still, and know that I am God. I will be exalted among the nations, I will be exalted in the earth!” The LORD of hosts is with us; the God of Jacob is our fortress.", reference: "Psalm 46:10-11" },
   { verse: "The Lord is my shepherd; I shall not want.", reference: "Psalm 23:1" },
-  { verse: "Do not be anxious about anything, but in every situation, by prayer and petition, with thanksgiving, present your requests to God.", reference: "Philippians 4:6" },
-  { verse: "And we know that in all things God works for the good of those who love him.", reference: "Romans 8:28" },
+  { verse: "do not be anxious about anything, but in everything by prayer and supplication with thanksgiving let your requests be made known to God.", reference: "Philippians 4:6" },
+  { verse: "And we know that for those who love God all things work together for good, for those who are called according to his purpose. For those whom he foreknew he also predestined to be conformed to the image of his Son, in order that he might be the firstborn among many brothers. And those whom he predestined he also called, and those whom he called he also justified, and those whom he justified he also glorified.", reference: "Romans 8:28-30" },
 ];
