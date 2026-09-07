b9747a83bbec4f73f45b328de905058a81d75e39
David <davidmitchellfootball@googlemail.com>
2026-09-08 00:40:30 +0100

    fix: remove children links from Tools navigation item

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -21,14 +21,6 @@ export const links: NavLink[] = [
   
   {
     label: "Tools",
-    href: "/tools/",
-    children: [
-      { label: "Bible Book Progress", href: "/tools/bibleBookProgress" },
-      { label: "Sanctify Sunday", href: "/tools/sanctifyingSunday" },
-      { label: "Wifi QR Code Generator", href: "/tools/qr-wifi" },
-      { label: "Link QR Code Generator", href: "/tools/qr-link" },
-
-      // { label: "Mortifying Sin", href: "/tools/mortifyingSin" },
-    ],
+    href: "/tools/"
   },
 ];
