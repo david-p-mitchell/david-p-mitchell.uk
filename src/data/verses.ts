1cd447bca5abb0b856545130cb9bc45828767e3a
David <30800534+david-p-mitchell@users.noreply.github.com>
2026-09-20 09:43:26 +0100

    Update verses.ts

--- src/data/verses.ts
+++ src/data/verses.ts
@@ -1,7 +1,7 @@
 export const verses = [
   { verse: "I can do all things through Christ who strengthens me.", reference: "Philippians 4:13" },
   { verse: "Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight.", reference: "Proverbs 3:5-6" },
-  { verse: "For I know the plans I have for you, declares the LORD, plans for welfare and not for evil, xto give you a future and a hope. Then you will call upon me and come and pray to me, and I will hear you. You will seek me and find me, when you seek me with all your heart.", reference: "Jeremiah 29:11-13" },
+  { verse: "For I know the plans I have for you, declares the LORD, plans for welfare and not for evil, to give you a future and a hope. Then you will call upon me and come and pray to me, and I will hear you. You will seek me and find me, when you seek me with all your heart.", reference: "Jeremiah 29:11-13" },
   { verse: "Be still, and know that I am God. I will be exalted among the nations, I will be exalted in the earth!” The LORD of hosts is with us; the God of Jacob is our fortress.", reference: "Psalm 46:10-11" },
   { verse: "The Lord is my shepherd; I shall not want.", reference: "Psalm 23:1" },
   { verse: "Do not be anxious about anything, but in everything by prayer and supplication with thanksgiving let your requests be made known to God.", reference: "Philippians 4:6" },
